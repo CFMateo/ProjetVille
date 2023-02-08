572c542ae87ded9e156f585e15d6611ed4befdcc
mformenace <mateolucascf@gmail.com>
2023-02-08 17:54:05 +0100

    poser le debut du site
    a faire: commencer avec des questions et reponses predefinis sur une page web ensuite apres que ca c caree utiliser folium

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run(debug=True)
-
+#idee: faizre des questions predefini zehma des rectangles zbien fait, qui fait le truc interactif avec plusieurs choix
+# qui ensuite on donne la possibilite de faire apparaitre les resultats sur une page web via folium
